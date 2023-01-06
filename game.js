61dec452bd0d6b13ae2667b5e2eb4cce128499f8
Roshith P <roshith844@gmail.com>
2023-01-06 12:51:52 +0530

    feat: User Interface

---
+++ game.js
@@ -0,0 +1,33 @@
+// board
+var blockSize = 25
+var rows = 20
+var cols = 20
+var board
+var context
+
+// snke head 
+ var snakeX = blockSize * 5
+ var snakeY = blockSize * 5
+
+ // food
+ var foodX = blockSize * 10
+ var FoodY = blockSize * 10
+
+
+window.onload = function(){
+     board = document.getElementById('board')
+     board.height = rows * blockSize
+     board.width = cols * blockSize
+     context = board.getContext('2d')
+     update()
+}
+function update(){
+     context.fillStyle = 'black'
+     context.fillRect(0, 0, board.width, board.height)
+
+context.fillStyle = 'lime'
+context.fillRect(snakeX, snakeY, blockSize, blockSize)
+
+context.fillStyle = 'red'
+context.fillRect(foodX, FoodY, blockSize, blockSize)
+}
