a53997e197a93ee3e93de8bb43392fac4e1d4094
Roshith P <roshith844@gmail.com>
2023-01-06 13:13:23 +0530

    feat: move box for snake with arrowkeys

--- game.js
+++ game.js
@@ -8,7 +8,8 @@ var context;
 // snke head
 var snakeX = blockSize * 5;
 var snakeY = blockSize * 5;
-
+var velocityX = 0;
+var velocityY = 0;
 // food
 var foodX = blockSize * 10;
 var FoodY = blockSize * 10;
@@ -18,21 +19,43 @@ window.onload = function () {
   board.height = rows * blockSize;
   board.width = cols * blockSize;
   context = board.getContext("2d");
-  placeFood()
-  update();
+  placeFood();
+  document.addEventListener("keyup", changeDirection);
+  //   update();
+  setInterval(update, 1000 / 10); //1
 };
 function update() {
   context.fillStyle = "black";
   context.fillRect(0, 0, board.width, board.height);
 
-  context.fillStyle = "lime";
-  context.fillRect(snakeX, snakeY, blockSize, blockSize);
-
   context.fillStyle = "red";
   context.fillRect(foodX, FoodY, blockSize, blockSize);
+
+  context.fillStyle = "lime";
+  snakeX += velocityX * blockSize;
+  snakeY += velocityY * blockSize;
+  context.fillRect(snakeX, snakeY, blockSize, blockSize);
 }
 
-function placeFood(){
-     foodX = Math.floor(Math.random() * cols)* blockSize
-     foodX = Math.floor(Math.random() * rows)* blockSize
+function placeFood() {
+  foodX = Math.floor(Math.random() * cols) * blockSize;
+  foodX = Math.floor(Math.random() * rows) * blockSize;
+}
+function changeDirection(e) {
+  if (e.code == "ArrowUp") {
+    velocityX = 0;
+    velocityY = -1;
+  }
+  if (e.code == "ArrowDown") {
+    velocityX = 0;
+    velocityY = 1;
+  }
+  if (e.code == "ArrowLeft") {
+    velocityX = -1;
+    velocityY = 0;
+  }
+  if (e.code == "ArrowRight") {
+    velocityX = 1;
+    velocityY = 0;
+  }
 }
