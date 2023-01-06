d727363b5d25b28606c7a1c20dbe300e047a8163
Roshith P <roshith844@gmail.com>
2023-01-06 12:56:57 +0530

    feat: show random box as food in snake game

--- game.js
+++ game.js
@@ -1,33 +1,38 @@
 // board
-var blockSize = 25
-var rows = 20
-var cols = 20
-var board
-var context
+var blockSize = 25;
+var rows = 20;
+var cols = 20;
+var board;
+var context;
 
-// snke head 
- var snakeX = blockSize * 5
- var snakeY = blockSize * 5
+// snke head
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
 
- // food
- var foodX = blockSize * 10
- var FoodY = blockSize * 10
+// food
+var foodX = blockSize * 10;
+var FoodY = blockSize * 10;
 
+window.onload = function () {
+  board = document.getElementById("board");
+  board.height = rows * blockSize;
+  board.width = cols * blockSize;
+  context = board.getContext("2d");
+  placeFood()
+  update();
+};
+function update() {
+  context.fillStyle = "black";
+  context.fillRect(0, 0, board.width, board.height);
 
-window.onload = function(){
-     board = document.getElementById('board')
-     board.height = rows * blockSize
-     board.width = cols * blockSize
-     context = board.getContext('2d')
-     update()
+  context.fillStyle = "lime";
+  context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+  context.fillStyle = "red";
+  context.fillRect(foodX, FoodY, blockSize, blockSize);
 }
-function update(){
-     context.fillStyle = 'black'
-     context.fillRect(0, 0, board.width, board.height)
 
-context.fillStyle = 'lime'
-context.fillRect(snakeX, snakeY, blockSize, blockSize)
-
-context.fillStyle = 'red'
-context.fillRect(foodX, FoodY, blockSize, blockSize)
+function placeFood(){
+     foodX = Math.floor(Math.random() * cols)* blockSize
+     foodX = Math.floor(Math.random() * rows)* blockSize
 }
