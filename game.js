2bf0ed5faf2a270a9dadf2dd3099a8795c8bc69c
Roshith P <roshith844@gmail.com>
2023-01-06 20:01:23 +0530

    feat: give snake length after getting food

--- game.js
+++ game.js
@@ -10,6 +10,8 @@ var snakeX = blockSize * 5;
 var snakeY = blockSize * 5;
 var velocityX = 0;
 var velocityY = 0;
+
+var snakeBody = [];
 // food
 var foodX = blockSize * 10;
 var FoodY = blockSize * 10;
@@ -30,11 +32,23 @@ function update() {
 
   context.fillStyle = "red";
   context.fillRect(foodX, FoodY, blockSize, blockSize);
-
+  if (snakeX == foodX && snakeY == FoodY) {
+    snakeBody.push([foodX, FoodY]);
+    placeFood();
+  }
+  for (let i = snakeBody.length - 1; i > 0; i--) {
+    snakeBody[i] = snakeBody[i - 1];
+  }
+  if (snakeBody.length) {
+    snakeBody[0] = [snakeX, snakeY];
+  }
   context.fillStyle = "lime";
   snakeX += velocityX * blockSize;
   snakeY += velocityY * blockSize;
   context.fillRect(snakeX, snakeY, blockSize, blockSize);
+  for (let i = 0; i < snakeBody.length; i++) {
+    context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+  }
 }
 
 function placeFood() {
@@ -42,19 +56,19 @@ function placeFood() {
   foodX = Math.floor(Math.random() * rows) * blockSize;
 }
 function changeDirection(e) {
-  if (e.code == "ArrowUp") {
+  if (e.code == "ArrowUp" && velocityY != 1) {
     velocityX = 0;
     velocityY = -1;
   }
-  if (e.code == "ArrowDown") {
+  if (e.code == "ArrowDown" && velocityY != -1) {
     velocityX = 0;
     velocityY = 1;
   }
-  if (e.code == "ArrowLeft") {
+  if (e.code == "ArrowLeft" && velocityX != 1) {
     velocityX = -1;
     velocityY = 0;
   }
-  if (e.code == "ArrowRight") {
+  if (e.code == "ArrowRight" && velocityX != -1) {
     velocityX = 1;
     velocityY = 0;
   }
